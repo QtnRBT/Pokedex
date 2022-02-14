d27610f408a98d9611c838d6a9151facd615708e
QUENTIN <36104147+QtnRBT@users.noreply.github.com>
2022-02-14 22:35:55 +0100

    Update server.js

--- api/config/server.js
+++ api/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '850142e7cde5354f1023cd4884aa2d91'),
+      secret: env('ADMIN_JWT_SECRET', 'someSecret'),
     },
   },
 });
